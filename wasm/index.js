515fadbb6a8e2d265b2abb0434471d43771695c8
Katsiaryna Kapylova <lologusenica@yandex.ru>
2025-08-20 21:05:16 +0300

    Updates

---
+++ wasm/index.js
@@ -0,0 +1,5 @@
+// Экспортируем createFiltersModule функцию из сгенерированного WASM файла
+import createFiltersModule from './filters.js';
+
+export { createFiltersModule };
+export default createFiltersModule;
